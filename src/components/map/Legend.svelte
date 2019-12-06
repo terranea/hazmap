63509df5dfd1a4d431a21682b78a38e9039438fd
jakoblo <jakoblo@pm.me>
2019-12-06 14:25:23 +0100

    gauge legend

--- src/components/map/Legend.svelte
+++ src/components/map/Legend.svelte
@@ -18,12 +18,36 @@
   li {
       display: flex; 
       align-items: center;
+      height: 20px;
+      line-height: 20px;
   }
 
   img {
       width: 20px;
       margin-right: 3px;
   }
+
+  .dot {
+  height: 12px;
+  width: 12px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  margin: 0;
+  margin-right: 5px;
+}
+
+.low {
+  background: #C58D3A;
+}
+
+.normal {
+  background: #52B04A;
+}
+
+.high {
+  background: #3A59C5;
+}
 
 </style>
 
@@ -36,4 +60,12 @@
 <li><img src="icons/storm-sm.png" alt="storm" />Storm</li>
 <li><img src="icons/picture.png" alt="picture" />Note/Picture Location</li>
 <li><img src="icons/aoi.png" alt="event aoi" />Event AOI</li>
+</ul>
+
+<span>Gauge Stations</span>
+<ul>
+<li><span class="dot normal"></span>normal water level</li>
+<li><span class="dot low"></span>low water level</li>
+<li><span class="dot high"></span>high water level</li>
+<li><span class="dot"></span>no current water level</li>
 </ul>
